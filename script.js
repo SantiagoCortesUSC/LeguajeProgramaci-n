0b040fac54b8c8917e17fdafd69f90beea5aacc8
SantiagoCortesUSC <144747168+SantiagoCortesUSC@users.noreply.github.com>
2023-11-29 20:15:05 -0500

    actualizacion 2

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ function cambiarContenido(contenido) {
     for (var i = 0; i < elementos.length; i++) {
         elementos[i].style.display = 'none';
     }
-    document.getElementById(contenido).style.display = 'block';
+
+    
+
+    var contenidoElement = document.getElementById(contenido);
+    if (contenidoElement) {
+        contenidoElement.style.display = 'block';
+
+        // Mostrar todos los elementos hijos de contenido
+        var hijos = contenidoElement.getElementsByTagName('div');
+        for (var j = 0; j < hijos.length; j++) {
+            hijos[j].style.display = 'block';
+        }
+    }
+    // document.getElementById(contenido).style.display = 'block';
 }
+
+
